9ef69f8f705249fec8c4cfa6c952a9cca78e0f09
Steve Calverley <steve.calverley@heffron.com.au>
2021-12-23 10:50:17 +1100

    Update prisma lib

--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = global.prisma || new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query']
+  });
 
-export default prisma;
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
