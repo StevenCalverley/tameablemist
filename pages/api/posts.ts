9a2ea7dca903017f75380449e985a36afb1e7746
Steve Calverley <steve.calverley@heffron.com.au>
2022-02-20 15:29:54 +1100

    Update post schema

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { prisma } from '../../lib/prisma';
 
-export default async function assetHandler(
+export default async function postController(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -17,8 +17,25 @@ export default async function assetHandler(
         res.status(500).json({ error: 'Error fetching posts' });
       }
       break;
+    case 'POST':
+      try {
+        const { title, content, excerpt } = req.body;
+
+        const newPost = await prisma.post.create({
+          data: {
+            title,
+            content,
+            excerpt
+          }
+        });
+        res.status(201).json({ ...newPost });
+      } catch (e) {
+        console.error('Request error', e);
+        res.status(500).json({ error: 'Error creating post' });
+      }
+      break;
     default:
-      res.setHeader('Allow', ['GET']);
+      res.setHeader('Allow', ['GET', 'POST']);
       res.status(405).end(`Method ${method} Not Allowed`);
       break;
   }
